df788fa31c25eaa7ed3fa873158812fe87fd6649
xiaobai7 <983911324@qq.com>
2018-01-03 18:08:22 +0800

    first commit

---
+++ src/Md5.php
@@ -0,0 +1,11 @@
+<?php
+namespace Xdm\Md5;
+class Md5{
+	public function make( $password){
+		return md5(md5($password));
+	}
+	public function check( $password,$value){
+		return md5(md5($password)) === $value;
+	}
+}   
+?>
---
+++ src/Md5Provider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Xdm\Md5;
+use Illuminate\Support\ServiceProvider;
+
+
+class Md5Provider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('md5',function(){
+            return new Md5();
+        });
+        // singletion();//单列
+    }
+}
